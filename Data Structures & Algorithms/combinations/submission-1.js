1ae7cf4fe149de13099c634f3f0ec7bf0f2ab69c
Yaroslav <90201472+zazazaza4@users.noreply.github.com>
2026-09-25 22:33:47 +0200

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number} k
+     * @return {number[][]}
+     */
+    combine(n, k) {
+        const res = []
+
+        this.helper(1, [], res, n, k)
+
+        return res 
+    }
+
+    helper(index, curr, combs, n, k) {
+        if (curr.length === k) {
+            combs.push([...curr])
+            
+            return
+        }
+
+        if (index > n) {
+            return
+        }
+
+        curr.push(index)
+        this.helper(index + 1, curr, combs, n, k)
+        curr.pop()
+
+        this.helper(index + 1, curr, combs, n, k)
+    }
+}
